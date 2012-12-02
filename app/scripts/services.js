72e037e18368257f8b7d91d8c7df6f1cd573d467
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2012-12-03 00:40:09 +0400

    Backend url changed

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,7 +2,7 @@
 
 var backendHostname = window.location.hostname == 'localhost'
     ? 'http://localhost\\:3000'
-    : 'http://barbudos-backend.eu01.aws.af.cm';
+    : 'http://barbudos.eu01.aws.af.cm';
 
 angular.module('dishesServices', ['ngResource']).factory('Dish', ['$resource', function ($resource) {
     return $resource(backendHostname + '/dishes/:dishId');
